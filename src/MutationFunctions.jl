e1e478f616fc825672e13fc7b3184c151468db1e
Nicolas Urcelay <nico_mx4@hotmail.com>
2022-03-08 01:44:14 -0600

    Add documentation to the function dither_mutation.

--- src/MutationFunctions.jl
+++ src/MutationFunctions.jl
@@ -1,5 +1,52 @@
 """
+    dither_mutation(A::AbstractMatrix, lower_limits::AbstractArray, upper_limits::AbstractArray, F::Real)
+Given a matrix `A` with `lower_limits` and `upper_limits` arrays, crates a new mutated
+matrix with a sacale factor `F`.
 
+# Parameters
+**`A::AbstractMatrix`**:
+
+Input matrix
+
+## **`lower_limits::AbstractArray`**:
+
+Array that contains all the minimun values
+that each individue could obtain.
+
+## **`upper_limits::AbstractArray`**:
+
+Array that contains all the maximum values
+that each individue could obtain.
+
+## **`F::Real`**:
+
+The scale factor, F ∈ (0,1+), is a positive real number that controls
+the rate at which the population evolves. While there is no upper limit on F,
+effective values are seldom greater than 1.0.
+(*Price, K., Storn, R. M., & Lampinen, J. A. (2006).
+Differential evolution: a practical approach to global optimization.
+Springer Science & Business Media.*)
+
+# Examples
+```julia-repl
+julia> A = 5 .*rand(5,3)
+5×3 Matrix{Float64}:
+ 2.48847   2.80436   1.94457
+ 4.04176   2.1029    2.66386
+ 4.05949   4.0124    1.02225
+ 0.428298  4.07771   4.83569
+ 2.92697   0.497032  4.71538
+```
+`A` is a matrix with minimun and maximum possible values equal to 0 and 5 respectively.
+```julia-repl
+julia> dither_mutation(A,[1,1,1],[5,5,5],1.3)
+5×3 Matrix{Float64}:
+ 3.49753  2.99707  1.3635
+ 7.51826  3.87943  3.53243
+ 1.73524  3.0894   3.78479
+ 4.55726  3.40921  1.96368
+ 3.89918  3.86246  1.69276
+```
 """
 function dither_mutation(A::AbstractMatrix, lower_limits::AbstractArray, upper_limits::AbstractArray, F::Real)
     
