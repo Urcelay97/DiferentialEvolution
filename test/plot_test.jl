00e1020e9b886db71fe2d342d168b37acd9e3f41
Nicolas Urcelay <nico_mx4@hotmail.com>
2022-03-19 03:07:19 -0600

    testing DE_JADE

--- test/plot_test.jl
+++ test/plot_test.jl
@@ -3,14 +3,15 @@ using DiferentialEvolution
 using PlotlyJS
 
 #Plots
-len=1000
-x = LinRange(0,pi,len)
-y = LinRange(0,pi,len)
+len=500
+x = LinRange(-600,600,len)
+y = LinRange(-600,600,len)
 X = ones(len)*x'
 Y = y*ones(len)'
-Z = Michalewicz.(X,Y)
-
-critic = DE_jither(Michalewicz,1000,500, [0,0], [pi,pi],0.9,0.5)
+Z = Eggholder.(X,Y)
+Eggholder(-2.5153506020027042e64, 1.79281509087981e64)
+@time critic1 = DE_JADE(Eggholder, 500, 25, [400,400], [600,600], 0.2, 1/20)
+@time critic2 = DE_dither(Eggholder, 1000, 500, [-512,-512], [512,512],0.9,0.5)
 p_init = contour(x =x, y= y,z = Z)
 crit = scatter(x=[critic[2][1]],y=[critic[2][2]], mode="markers",color=2)
 plot([p_init,crit])
