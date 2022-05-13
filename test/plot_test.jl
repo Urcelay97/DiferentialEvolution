d8543c9af1286ff5d3bb9bca7ab064fb7b3202ae
Nicolas Urcelay <nico_mx4@hotmail.com>
2022-05-13 18:34:23 -0500

    add tests

--- test/plot_test.jl
+++ test/plot_test.jl
@@ -3,6 +3,7 @@ using DiferentialEvolution
 using PlotlyJS
 
 #Plots
+#=
 len=500
 x = LinRange(-600,600,len)
 y = LinRange(-600,600,len)
@@ -15,3 +16,15 @@ Eggholder(-2.5153506020027042e64, 1.79281509087981e64)
 p_init = contour(x =x, y= y,z = Z)
 crit = scatter(x=[critic[2][1]],y=[critic[2][2]], mode="markers",color=2)
 plot([p_init,crit])
+=#
+
+f(x) = sinc(x)
+x = -5:0.1:5
+y = f.(x)
+g(par...) = par*[@. x^i for i in 1:length(par)]
+error(a,b,c,d,e,f,h,i,k,l,m,n) = sum(abs.(y-g(a,b,c,d,e,f,h,i,k,l,m,n)))
+res = DiferentialEvolution.DE_JADE(error,1000,500,[-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100],[100,100,100,100,100,100,100,100,100,100,100,100],0.15,0.2)
+
+a1 = scatter(x=x, y=y)
+a2 = scatter(x=x, y=g(res[2]...))
+plot([a1,a2])
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,31 +7,86 @@ using Test
     @testset "Classic Diferential Evolution" begin
 
         @testset "2-Dimensioanl tests" begin
-
-        end
-
-
-        @testset "5-Dimensional tests" begin
-
-            AckleyT = DE_classic(Ackley,1000,300, [-32.768,-32.768,-32.768,-32.768,-32.768], [32.768,32.768,32.768,32.768,32.768],0.9,0.5)
-            SchwefelT = DE_classic(Schwefel,1000,300, [-500,-500,-500,-500,-500], [500,500,500,500,500],0.9,0.5)
-            GriewankT = DE_classic(Griewank,1000,300, [-600,-600,-600,-600,-600], [600,600,600,600,600],0.9,0.5)
-            RosenbrockT = DE_classic(Rosenbrock,1000,300, [-5,-5,-5,-5,-5], [10,10,10,10,10],0.9,0.5)
-            MichalewiczT = DE_classic(Michalewicz,1000,300, [0,0,0,0,0], [pi,pi,pi,pi,pi],0.9,0.5)
+            AckleyT = DE_classic(Ackley,1000,300, [-32.768,-32.768], [32.768,32.768],0.9,0.5)
+            SchwefelT = DE_classic(Schwefel,1000,300, [-500,-500], [500,500],0.9,0.5)
+            GriewankT = DE_classic(Griewank,1000,300, [-600,-600], [600,600],0.9,0.5)
+            RosenbrockT = DE_classic(Rosenbrock,1000,300, [-5,-5], [10,10],0.9,0.5)
+            MichalewiczT = DE_classic(Michalewicz,1000,300, [0,0], [pi,pi],0.9,0.5)
 
             #Ackley     critic value = 0   ///  parameters = [0,0,0,0,0]
             @test isapprox(AckleyT[1],0, atol=1e-8)
             #Schwefel   critic value = -418.9829 d   ///  parameters = [420.9687,420.9687,420.9687,420.9687,420.9687]
-            d = 5
-            @test isapprox(SchwefelT[1],-418.9829*d, atol=1e-4)
+            D = 2
+            @test isapprox(SchwefelT[1],-418.9829*D, atol=1e-4)
             #Griewank   critic value = 0   ///  parameters = [0,0,0,0,0]
             @test isapprox(GriewankT[1],0, atol=1e-8)
             #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            @test isapprox(RosenbrockT[1],0, atol=1e-8)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            @test isapprox(MichalewiczT[1],-1.8013, atol=1e-4)
+        end
+
+
+        @testset "5-Dimensional tests" begin
+            D = 5
+            AckleyT = DE_classic(Ackley,1000,300, [-32.768 for i in 1:D], [32.768 for i in 1:D],0.9,0.5)
+            SchwefelT = DE_classic(Schwefel,1000,300, [-500 for i in 1:D], [500 for i in 1:D],0.9,0.5)
+            GriewankT = DE_classic(Griewank,1000,300, [-600 for i in 1:D], [600 for i in 1:D],0.9,0.5)
+            RosenbrockT = DE_classic(Rosenbrock,1500,300, [-5 for i in 1:D], [10 for i in 1:D],0.9,0.5)
+            MichalewiczT = DE_classic(Michalewicz,1000,300, [0 for i in 1:D], [pi for i in 1:D],0.9,0.5)
+
+            #Ackley     critic value = 0   ///  parameters = [0,0,0,0,0]
+            @test isapprox(AckleyT[1],0, atol=1e-8)
+            #Schwefel   critic value = -418.9829 d   ///  parameters = [420.9687,420.9687,420.9687,420.9687,420.9687]
+            @test isapprox(SchwefelT[1],-418.9829*D, atol=1e-4)
+            #Griewank   critic value = 0   ///  parameters = [0,0,0,0,0]
             @test isapprox(GriewankT[1],0, atol=1e-8)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            @test isapprox(RosenbrockT[1],0, atol=1e-8)
             #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
             @test isapprox(MichalewiczT[1],-4.687658, atol=1e-6)
         end
-        
+
+        @testset "15 - Dimensional tests" begin
+            D = 15
+            AckleyT = DE_classic(Ackley,3000,700, [-32.768 for i in 1:D], [32.768 for i in 1:D],0.9,0.5)
+            SchwefelT = DE_classic(Schwefel,3000,700, [-500 for i in 1:D], [500 for i in 1:D],0.9,0.5)
+            GriewankT = DE_dither(Griewank,3000,700, [-600 for i in 1:D], [600 for i in 1:D],0.9,0.5)
+            RosenbrockT = DE_dither(Rosenbrock,3000,700, [-5 for i in 1:D], [10 for i in 1:D],0.9,0.5)
+            #MichalewiczT = DE_classic(Michalewicz,1000,300, [0 for i in 1:D], [pi for i in 1:D],0.9,0.5)
+
+            #Ackley     critic value = 0   ///  parameters = [0,0,0,0,0]
+            @test isapprox(AckleyT[1],0, atol=1e-2)
+            #Schwefel   critic value = -418.9829 d   ///  parameters = [420.9687,420.9687,420.9687,420.9687,420.9687]
+            @test isapprox(SchwefelT[1],-418.9829*D, atol=1e-2)
+            #Griewank   critic value = 0   ///  parameters = [0,0,0,0,0]
+            @test isapprox(GriewankT[1],0, atol=1e-2)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            @test isapprox(RosenbrockT[1],0, atol=1e-2)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            #@test isapprox(MichalewiczT[1],-12.952351, atol=1e-6)
+        end
+#=
+        @testset "30 - Dimensional tests" begin
+            D = 30
+            AckleyT = DE_classic(Ackley,5000,700, [-32.768 for i in 1:D], [32.768 for i in 1:D],0.9,0.5)
+            SchwefelT = DE_classic(Schwefel,5000,700, [-500 for i in 1:D], [500 for i in 1:D],0.9,0.5)
+            GriewankT = DE_classic(Griewank,5000,700, [-600 for i in 1:D], [600 for i in 1:D],0.9,0.5)
+            RosenbrockT = DE_classic(Rosenbrock,5000,700, [-5 for i in 1:D], [10 for i in 1:D],0.9,0.5)
+            #MichalewiczT = DE_classic(Michalewicz,1000,300, [0 for i in 1:D], [pi for i in 1:D],0.9,0.5)
+
+            #Ackley     critic value = 0   ///  parameters = [0,0,0,0,0]
+            @test isapprox(AckleyT[1],0, atol=1e-2)
+            #Schwefel   critic value = -418.9829 d   ///  parameters = [420.9687,420.9687,420.9687,420.9687,420.9687]
+            @test isapprox(SchwefelT[1],-418.9829*D, atol=1e-2)
+            #Griewank   critic value = 0   ///  parameters = [0,0,0,0,0]
+            @test isapprox(GriewankT[1],0, atol=1e-2)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            @test isapprox(RosenbrockT[1],0, atol=1e-2)
+            #Rosenbrock critic value = 0   ///  parameters = [1,1,1,1,1]
+            #@test isapprox(MichalewiczT[1],-4.687658, atol=1e-6)
+        end
+    =#  
     end
 
     @testset "Dither Diferential Evolution" begin
